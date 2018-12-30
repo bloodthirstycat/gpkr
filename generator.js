b0b08f5ed8a1053c43ece3dfcb17168359e9276b
Robert Niemiec <robercik10192@gmail.com>
2018-12-30 16:14:31 +0100

    First commit

---
+++ generator.js
@@ -0,0 +1,318 @@
+var aktorzy_male = [
+"Mikołaj Roznerski",
+"Krzysztof Czeczot",
+"Paweł Domagała",
+"Łukasz Garlicki",
+"Jan Frycz",
+"Tomasz Kot",
+"Piotr Adamczyk",
+"Wojciech Mecwaldowski",
+"Tomasz Karolak",
+"Maciej Stuhr",
+"Borys Szyc",
+"Zbigniew Zamachowski",
+"Rafał Królikowski"
+];
+
+var aktorzy_female = [
+"Weronika Rosati",
+"Barbara Kurdej-Szatan",
+"Anna Dereszowska",
+"Danuta Stenka",
+"Katarzyna Bujakiewicz",
+"Sonia Bohosiewicz",
+"Małgorzata Kożuchowska",
+"Agnieszka Dygant",
+"Magdalena Różczka",
+"Anna Dereszowska",
+"Marta Żmuda Trzebiatowska",
+"Edyta Olszówka",
+"Małgorzata Socha"
+];
+
+var imiona_male = [
+"Jakub",
+"Kacper",
+"Filip",
+"Szymon",
+"Jan",
+"Antoni",
+"Michał",
+"Wojciech",
+"Mateusz",
+"Bartosz",
+"Adam",
+"Franciszek",
+"Piotr",
+"Mikołaj",
+"Wiktor",
+"Igor",
+"Marcel",
+"Dawid",
+"Alan",
+"Oliwier",
+"Maciej",
+"Oskar",
+"Karol",
+"Tomasz",
+"Maksymilian",
+"Dominik",
+"Stanisław",
+"Miłosz",
+"Paweł",
+"Hubert",
+"Krzysztof",
+"Kamil",
+"Patryk",
+"Nikodem",
+"Fabian",
+"Bartłomiej",
+"Leon",
+"Sebastian",
+"Julian",
+"Tymoteusz",
+"Gabriel",
+"Tymon",
+"Krystian",
+"Ksawery",
+"Adrian",
+"Ignacy",
+"Łukasz",
+"Błażej",
+"Marcin"];
+
+var imiona_female = [
+"Lena",
+"Julia",
+"Zuzanna",
+"Maja",
+"Zofia",
+"Amelia",
+"Hanna",
+"Aleksandra",
+"Wiktoria",
+"Natalia",
+"Oliwia",
+"Alicja",
+"Nikola",
+"Maria",
+"Emilia",
+"Anna",
+"Nadia",
+"Gabriela",
+"Martyna",
+"Antonina",
+"Milena",
+"Magdalena",
+"Laura",
+"Weronika",
+"Karolina",
+"Pola",
+"Agata",
+"Kornelia",
+"Liliana",
+"Iga",
+"Michalina",
+"Patrycja",
+"Paulina",
+"Kinga",
+"Nina",
+"Jagoda",
+"Marta",
+"Katarzyna",
+"Joanna",
+"Małgorzata",
+"Klaudia",
+"Helena",
+"Barbara",
+"Dominika",
+"Kaja",
+"Blanka",
+"Marcelina",
+"Izabela",
+"Ewa",
+"Łucja"];
+
+var zawod = [
+"grafik",
+"pracownik biurowy",
+"specjalista ds. pr",
+"urzędnik państwowy",
+"lekarz",
+"specjalista ds. zakupów",
+"programista",
+"ekonomista",
+"architekt wnętrz / krajobrazu",
+"inżynier środowiska",
+"specjalista ds. marketingu",
+"programista baz danych",
+"prawnik",
+"webmaster",
+"automatyk",
+"specjalista ds. ochrony środowiska",
+"projektant wzornictwa",
+"laborant",
+"dietetyk",
+"specjalista ds. transportu",
+"tester aplikacji",
+"pedagog",
+"dyrektor ds.administracyjnych",
+"spedytor",
+"koordynator robót budowlanych",
+"specjalista ds. turystyki",
+"specjalista ds. bhp",
+"kurator sądowy",
+"key account manager",
+"dyrektor ds. rozwoju",
+"koordynator sprzedaży",
+"specjalista ds. obsługi klienta",
+"technolog",
+"analityk rynku",
+"fakturzystka",
+"agent celny",
+"księgowa",
+"administrator danych osobowych",
+"konstruktor",
+"tłumacz",
+"specjalista ds. planowania produkcji",
+"project manager",
+"adwokat",
+"przedstawiciel handlowy",
+"doradca zawodowy",
+"architekt budownictwa",
+"dyrektor ds. logistyki",
+"dyrektor ds. personalnych",
+"pełnomocnik ds. jakości",
+"farmaceuta",
+"asystentka zarządu",
+"geodeta",
+"copywriter",
+"specjalista ds. reklamacji",
+"projektant it",
+"doradca finansowy",
+"dyrektor ds. finansowych",
+"recepcjonistka",
+"inżynier budowy",
+"dziennikarz",
+"lektor",
+"administrator",
+"trener",
+"specjalista ds. inwestycji",
+"handlowiec",
+"pracownik socjalny",
+"agenta nieruchomości",
+"pilot wycieczek",
+"product manager",
+"inżynier utrzymania ruchu",
+"office manager",
+"konsultant ds. wdrożeń",
+"projektant konstrukcji budowlanych",
+"notariusz",
+"rzeczoznawca",
+"programista plc",
+"specjalista ds. funduszy unijnych",
+"przedstawiciel medyczny",
+"kierownik magazynu",
+"doradca techniczno-handlowy",
+"specjalista ds. badań i rozwoju",
+"telemarketer",
+"specjalista ds. windykacji",
+"prezenter",
+"kosztorysant",
+"radca prawny",
+"dyrektor ds. marketingu",
+"dyrektor ds. sprzedaży",
+"kierownik punktu sprzedaży",
+"specjalista ds. dostaw",
+"inżynier ds. wdrażania produkcji",
+"specjalista ds. ofertowania",
+"doradca podatkowy",
+"dyrektor ds. produkcji",
+"specjalista ds. pozycjonowania",
+"nauczyciel",
+"kierownik kontraktu",
+"analityk systemów",
+"specjalista ds. kadr i płac",
+"specjalista ds. rekrutacji"
+];
+
+var kto = [
+"ojciec",
+"matka",
+"koleżanka",
+"pies",
+"kot",
+"chłopak",
+"babcia",
+"dziadek",
+"chomik",
+"brat",
+"kuzyn"
+];
+
+var co_sie_dzieje = [
+"zdradza ją z jej najlepszą koleżanką",
+"ginie w wyniku eksplozji atomowej",
+"ginie potrącony / potrącona przez samochód",
+"ginie w niewyjaśnionych okolicznościach",
+"zostaje porwany / porwana przez UFO",
+"okazuje się być kosmitą",
+"strzela focha",
+"przestaje się do niej odzywać",
+"okazuje się mieć zdolności paranormalne",
+"okazuje się mieć zdolności magiczne",
+"okazuje zdolność rozmawiania ze zwierzętami",
+"okazuje zdolność latania"
+];
+
+var co_postanawia = [
+"rzucić pracę",
+"poznać nowego faceta",
+"udać się do psychiatry",
+"nauczyć się czegoś nowego",
+"skoczyć z wieżowca",
+"wyjechać za granicę",
+"nauczyć się karate",
+"zostać mnichem tybetańskim",
+"wspiąć się na Mount Everest"];
+
+var ale_czy_co = [
+"ich związek przetrwa próbę czasu",
+"Warszawa obroni się przed hordą zmutowanych zombie",
+"wyborcy zagłosują na zmiennokształtnego jaszczura w ciele człowieka",
+"meteory zniszczą ziemię",
+"Polacy dostaną się do Mistrzostw Świata",
+"walcząc z demonami dowiedzą się prawdy o sobie",
+"pokonają zło, jakie ciąży nad ziemią"];
+
+function losuj(tablica){
+	return tablica[Math.floor(Math.random()*tablica.length)];
+}
+
+function odmien(imie){
+	if(imie == "Paweł"){
+		return "Pawła";
+	}else if(imie == "Antoni"){
+		return "Antoniego";
+	}else if(imie == "Ksawery"){
+		return "Ksawerego";
+	}else if(imie == "Kacper"){
+		return "Kacpra";
+	}else if(imie == "Ignacy"){
+		return "Ignacego";
+	}else{
+		return imie + "a";
+	}
+}
+
+function generuj(){
+	var zdanie = losuj(imiona_female) + " (" + losuj(aktorzy_female) + ") " + " pracuje jako " + losuj(zawod) + " i wiedzie szczęśliwe życie u boku " + odmien(losuj(imiona_male)) + " (" + losuj(aktorzy_male) + "), jednak gdy jej " + losuj(kto) + " " + losuj(co_sie_dzieje) + " postanawia " + losuj(co_postanawia) + ". Przy okazji poznaje " + odmien(losuj(imiona_male)) + " (" + losuj(aktorzy_male) + "). Ale czy " + losuj(ale_czy_co) + "?";
+	document.getElementById("gen").innerText = zdanie;
+}
+
+function pobierz(kto){
+	switch(kto){
+		case 'aktorzy_male':
+		
+	}
+}
